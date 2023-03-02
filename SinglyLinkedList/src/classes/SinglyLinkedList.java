4baeb7f8de59b3138c5a491ea719958730452ad3
Calin <chelceacalin1@gmail.com>
2023-03-02 16:56:08 +0200

    Delete by Capacity and insert end

--- SinglyLinkedList/src/classes/SinglyLinkedList.java
+++ SinglyLinkedList/src/classes/SinglyLinkedList.java
@@ -8,7 +8,6 @@ public class SinglyLinkedList {
     public void insertBeginning(Avion a){
         Node aux=new Node(a);
         aux.next=null;
-
         if(head==null){
            head=aux;
         }
@@ -17,12 +16,45 @@ public class SinglyLinkedList {
             head=aux;
         }
     }
+    public void insertFinal(Avion a){
+        Node newNode=new Node(a);
 
+        Node aux=head;
+        if(head==null){
+            insertBeginning(a);
+        }
+        else {
+            while(aux.next!=null)
+                aux=aux.next;
 
+            aux.next=newNode;
+        }
+    }
+    public void DeleteCapacity(int capacitate){
+        Node aux=head;
+       if(head==null) {
+           System.out.println("List is empty!");
+           return;
+       };
+
+        if(aux.avion.capacitate==capacitate){
+            head=head.next;
+        }
+        else {
+            while(aux.next!=null&&aux.next.avion.capacitate!=capacitate)
+                aux=aux.next;
+
+            Node temp=aux;
+            if(aux.next.avion.capacitate==capacitate){
+                temp=aux.next;
+                aux.next=temp.next;
+            }
+        }
+    }
     public void print(){
         Node aux=head;
         while(aux!=null){
-          Avion a=aux.avion;
+            Avion a=aux.avion;
             System.out.println(aux.avion.toString()+" \n");
             aux=aux.next;
         }
--- SinglyLinkedList/src/main/main.java
+++ SinglyLinkedList/src/main/main.java
@@ -9,8 +9,23 @@ public class main {
 
         Avion a1=new Avion("Avion1",1);
         Avion a2=new Avion("Avion2",2);
+        Avion a3=new Avion("Avion3",3);
+        Avion a4=new Avion("Avion4",4);
+        Avion a5=new Avion("Avion5",5);
+        Avion a6=new Avion("Avion6",6);
+        Avion a7=new Avion("Avion7",7);
+
+
         SinglyLinkedList list=new SinglyLinkedList();
         list.insertBeginning(a1);
+        list.insertBeginning(a2);
+        list.insertFinal(a3);
+        list.insertFinal(a4);
+//
+//        list.StergeCapactitate(1);
+//        list.StergeCapactitate(3);
+//        list.StergeCapactitate(4);
+      list.StergeCapactitate(2);
 
         list.print();
 
